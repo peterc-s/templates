a1e1769e25c1490eb6715eb4baeb8c190a38cc7d
peterc-s <petercs@purelymail.com>
2026-02-09 16:29:32 +0000

    Move registry examples over to channel tarballs

--- rust-web-server/flake.nix
+++ rust-web-server/flake.nix
@@ -2,7 +2,7 @@
   description = "A Rust web server including a NixOS module";
 
   # Nixpkgs / NixOS version to use.
-  inputs.nixpkgs.url = "nixpkgs/nixos-21.05";
+  inputs.nixpkgs.url = "https://channels.nixos.org/nixos-21.05/nixexprs.tar.xz";
 
   inputs.import-cargo.url = github:edolstra/import-cargo;
 
